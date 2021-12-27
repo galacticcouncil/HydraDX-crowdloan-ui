6870406b443a4e0df09c56deb1ae960e74fb6360
Matej Sima <matej.sima@stove-labs.com>
2021-12-27 20:50:35 +0100

    disable button when api is not ready

--- src/components/AccountBar/ContributionForm.tsx
+++ src/components/AccountBar/ContributionForm.tsx
@@ -80,7 +80,9 @@ export const ContributionForm = ({
                     }
                 />
 
-                <button type='submit'>
+                <button 
+                    type='submit'
+                    disabled={!apiReady}>
                     {!apiReady
                         ? 'Connecting...'
                         : (
