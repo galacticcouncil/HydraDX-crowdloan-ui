6869d2f2edf3a1433ca40a46392ad09112bab00c
Valery Gantchev <v@lery.nl>
2022-03-10 10:41:23 +0100

    add HDX vesting scripts for hydra and snek crowdloans

---
+++ scripts/crowdloan-rewards/common.ts
@@ -0,0 +1,75 @@
+import BigNumber from "bignumber.js"
+
+export type DynamicVestingInfo = {
+  destination: string,
+  schedule: {
+      amountToBeVested: string,
+      start: string,
+      period: string,
+      per_period: string,
+      period_count: string 
+  }
+}
+
+export const generateVestings = function(
+  startBlock: string,
+  endBlock: string,
+  address: string,
+  rewards: string,
+  triple: boolean
+): DynamicVestingInfo[] {
+    const vestingDuration = new BigNumber(endBlock).minus(new BigNumber(startBlock));
+
+    const totalRewards = triple ? new BigNumber(rewards).multipliedBy(3) : rewards;
+
+    const rewardsPerPeriodFloat = new BigNumber(totalRewards).dividedBy(vestingDuration);
+
+    const rewardsSumOfDecimalAmounts = 
+        new BigNumber(rewardsPerPeriodFloat.modulo(1))
+        .multipliedBy(vestingDuration)
+        .decimalPlaces(0, BigNumber.ROUND_UP);
+
+    const rewardsPerPeriodFixed = 
+        new BigNumber(rewardsPerPeriodFloat)
+        .decimalPlaces(0, 1);
+
+    const rewardsMinusSumOfDecimals = 
+        new BigNumber(rewards)
+        .minus(rewardsSumOfDecimalAmounts);
+
+    return [
+        // In the first period we distribute the rounded sum of all decimal amounts
+        {
+            destination: address,
+            schedule: {
+                amountToBeVested: rewardsSumOfDecimalAmounts.toString(),
+                start: startBlock,
+                period: '1',
+                per_period: rewardsSumOfDecimalAmounts.toString(),
+                period_count: '1'
+            }
+            
+        },
+        // For every period we distribute rewards per period rounded down to fixed point
+        {
+            destination: address,
+            schedule: {
+                amountToBeVested: rewardsMinusSumOfDecimals.toString(),
+                start: startBlock,
+                period: '1',
+                per_period: rewardsPerPeriodFixed.toString(),
+                period_count: vestingDuration.toString()
+            }
+        }
+    ]
+};
+
+export const writeToFS = function(path: String, vestingBatch: DynamicVestingInfo[]) {
+  const fs = require('fs')
+
+  fs.writeFile (path, JSON.stringify(vestingBatch, null, 4), function(err) {
+          if (err) throw err
+          console.log('complete')
+      }
+  );
+}
