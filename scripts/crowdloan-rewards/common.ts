d46a6b4fd9c5e97910dcbb8b055c5ca4efe8248a
Valery Gantchev <v@lery.nl>
2022-03-17 15:06:08 +0100

    Revert "try to tackle existential deposit by higher vesting in first period"
    This reverts commit b9c37dea1a2549fdfbd6762e054318e484b73f74.

--- scripts/crowdloan-rewards/common.ts
+++ scripts/crowdloan-rewards/common.ts
@@ -18,8 +18,6 @@ export const generateVestings = function(
   rewards: string,
   triple: boolean
 ): DynamicVestingInfo[] {
-    const start = new BigNumber(startBlock);
-
     const vestingDuration = new BigNumber(endBlock).minus(new BigNumber(startBlock));
 
     const totalRewards = triple ? new BigNumber(rewards).multipliedBy(3) : new BigNumber(rewards);
@@ -31,42 +29,36 @@ export const generateVestings = function(
         .multipliedBy(vestingDuration)
         .decimalPlaces(0, BigNumber.ROUND_UP);
 
-    const rewardsPerPeriodFixed = rewardsPerPeriodFloat.decimalPlaces(0, 1);
+    const rewardsPerPeriodFixed = 
+        rewardsPerPeriodFloat
+        .decimalPlaces(0, 1);
 
-    const rewardsMinusSumOfDecimals = totalRewards.minus(rewardsSumOfDecimalAmounts);
+    const rewardsMinusSumOfDecimals = 
+        totalRewards
+        .minus(rewardsSumOfDecimalAmounts);
 
-    // First period
-    const amountFirst = rewardsSumOfDecimalAmounts.plus(rewardsPerPeriodFixed).toString();
-    const startFirst = start.toString();
-
-    // Other periods
-    const amountOther = rewardsMinusSumOfDecimals.minus(rewardsPerPeriodFixed).toString();
-    const startOther = start.plus(1).toString();
-    const perPeriodOther = rewardsPerPeriodFixed.toString();
-    const periodCountOther = vestingDuration.minus(1).toString();
-    
     return [
-        // For the first period we distribute rewardsPerPeriodFixed plus the rounded sum of all decimal amounts
+        // In the first period we distribute the rounded sum of all decimal amounts
         {
             destination: address,
             schedule: {
-                amountToBeVested: amountFirst,
-                start: startFirst,
+                amountToBeVested: rewardsSumOfDecimalAmounts.toString(),
+                start: startBlock,
                 period: '1',
-                per_period: amountFirst,
+                per_period: rewardsSumOfDecimalAmounts.toString(),
                 period_count: '1'
             }
             
         },
-        // For every other period we distribute rewards per period rounded down to fixed point
+        // For every period we distribute rewards per period rounded down to fixed point
         {
             destination: address,
             schedule: {
-                amountToBeVested: amountOther,
-                start: startOther,
+                amountToBeVested: rewardsMinusSumOfDecimals.toString(),
+                start: startBlock,
                 period: '1',
-                per_period: perPeriodOther,
-                period_count: periodCountOther
+                per_period: rewardsPerPeriodFixed.toString(),
+                period_count: vestingDuration.toString()
             }
         }
     ]
